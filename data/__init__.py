414673ec2e23375d0314aa8aebc7c2e7194cb93d
smchoi <saemee0007@gmail.com>
2023-06-01 06:32:08 +0000

    feat: Calculate and store task2 features

--- data/__init__.py
+++ data/__init__.py
@@ -12,13 +12,9 @@ SUPPORTED_DATASETS = {
     "task2": Task2Loader
 }
 
-def get_dataset(args, aug=None, return_label=False):
+def get_dataset(args):
     dataset_name = args.dataset
-    noise_type = args.noise_type
-    openset_ratio = args.openset_ratio
-    closeset_ratio = args.closeset_ratio
     if dataset_name not in SUPPORTED_DATASETS:
         raise NotImplementedError("Dataset {} does not exist.".format(dataset_name))
     if (dataset_name == 'task1') or (dataset_name == 'task2'):
         return SUPPORTED_DATASETS[dataset_name](args.data_path)
-    return SUPPORTED_DATASETS[dataset_name](aug=aug,noise_type=noise_type, openset_ratio=openset_ratio, closeset_ratio=closeset_ratio, return_label=return_label)
